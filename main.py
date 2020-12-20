2be3c75984c26e39a105422eedb5593eb877198a
isadarve <71551109+isadarve@users.noreply.github.com>
2020-12-20 19:32:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from pyspark.ml.feature import VectorAssembler
 from tools import kmeans_info, correlation_matrix, one_hot_encoder, apply_LinearRegression, apply_TreeRegressor
 
 import sys
-from pyspark.ml.regression import DecisionTreeRegressor, RandomForestRegressor, LinearRegression, \
-    GeneralizedLinearRegression, GBTRegressor
-
+from pyspark.ml.regression import DecisionTreeRegressor, RandomForestRegressor, LinearRegression
+    
 spark = SparkSession.builder.appName("bd_spark.com").master('local[4]').getOrCreate()
 file = open('bd_project/output.txt', 'w')
 
@@ -15,122 +14,74 @@ file = open('bd_project/output.txt', 'w')
 forbidden_cols = (
 'ArrTime', 'ActualElapsedTime', 'TaxiIn', 'AirTime', 'Diverted', 'CarrierDelay', 'WeatherDelay', 'NASDelay',
 'SecurityDelay', 'LateAircraftDelay')
-noinfo_cols = ('Cancelled', 'CancellationCode', 'FlightNum', 'TailNum', 'Year', 'CRSDepTime')
-txout = ('TaxiOut')
-cols_OHE = ['OriginOHE', 'DestOHE', 'UniqueCarrierOHE']
-# reading data from csv
-# df_raw = spark.read.option('inferSchema',True).option('header',True).csv('2008.csv')
+noinfo_cols = ('Cancelled','CancellationCode','FlightNum','TailNum','CRSDepTime', 'Origin', 'Dest', 'DayofMonth', 'DayOfWeek', 'CRSElapsedTime')
 
+# reading data from csv
 df_raw = spark.read.option('inferSchema', True).option('header', True).csv(sys.argv[1])
 
 for file in sys.argv[2:]:
     df = spark.read.option('inferSchema', True).option('header', True).csv(file)
     df_raw = df_raw.union(df)
+
 # dropping forbidden variables
 df = df_raw.drop(*forbidden_cols)
+
 # dropping variables that we consider they dont give any valuable info
 df = df.filter((df.Cancelled != 1))  # dropping rows with cancelled flights
 df = df.drop(*noinfo_cols)
 
-# combining columns to get planned speed of the plane
+# replace 'NA' por None values
+df = df.replace('NA', None)
 
-df = df.withColumn('TaxiOut', F.col('TaxiOut').cast('integer'))
-
-df = df.withColumn('TaxiOut', F.when(F.col('TaxiOut').isNull(), 15).otherwise(F.col('TaxiOut')))
-df.show(5)
-# casting columns to integer
+# convert strings to integer
 df = df.withColumn('DepDelay', F.col('DepDelay').cast('integer')) \
     .withColumn('ArrDelay', F.col('ArrDelay').cast('integer')) \
-    .withColumn('DepDelay', F.col('DepDelay').cast('integer')) \
     .withColumn('Distance', F.col('Distance').cast('integer')) \
     .withColumn('CRSArrTime', F.col('CRSArrTime').cast('integer')) \
-    .withColumn('CRSElapsedTime', F.col('CRSElapsedTime').cast('integer')) \
-    .withColumn('DayofMonth', F.col('DayofMonth').cast('integer')) \
-    .withColumn('DayOfWeek', F.col('DayOfWeek').cast('integer')) \
-    .withColumn('Month', F.col('Month').cast('integer')) \
+    .withColumn('TaxiOut',F.col('TaxiOut').cast('integer'))\
     .withColumn('DepTime', F.col('DepTime').cast('integer'))
 
-df = df.withColumn('CRSPlanSpeed', F.expr('Distance / CRSElapsedTime'))
-# drop rows with NA in some column
-df = df.na.drop('any')
-# map strings to numbers with string indexer
-string_cols = ['UniqueCarrier', 'Origin', 'Dest']
+# Replace Null values in the column 'TaxiOut' with the mean of the column taking onto account all years' datasets
+df = df.withColumn('TaxiOut', F.when(F.col('TaxiOut').isNull(), 15).otherwise(F.col('TaxiOut')))
+
+# drop rows with NA values
+df = df.dropna('any')
+
+# perform OneHotEncoder for cathegorical features
+string_cols = ['Year', 'UniqueCarrier', 'Month']
+cols_OHE = ['YearOHE', 'UniqueCarrierOHE', 'MonthOHE']
 df = one_hot_encoder(df=df, inputCols=string_cols, outputCols=cols_OHE)
-# correlation matrix
-file.write('Correlation Matrix\n')
-int_cols = ['Month', 'DayofMonth', 'DayOfWeek', 'DepTime', 'CRSArrTime', 'DepDelay', 'CRSPlanSpeed',
-            'Distance', 'CRSElapsedTime', 'TaxiOut', 'ArrDelay']
-info = correlation_matrix(df, int_cols)
-file.write(info)
-df_pre = df
-# after the correlation analysis dropping variables with correlation less than 0.05
-drop_low_corr_cols = ['DayOfWeek', 'DayOfMonth', 'CRSPlanSpeed', 'Distance', 'CRSArrTimeOHE', 'DepTimeOHE']
-df = df.drop(*drop_low_corr_cols)
-# showing final schema
-df.printSchema()
-df.show(5)
-file.write('\n Final Schema : \n' + df._jdf.schema().treeString())
+
+# change format of 'DepTime' and 'CRSArrTime' from hhmm to minutes
+df = df.withColumn('DepTime', F.when(((0 <= df['DepTime']) & (df['DepTime'] <= 2400)),F.floor(df['DepTime']/100) * 60 + df['DepTime']%100).otherwise(-1))
+df = df.withColumn('CRSArrTime', F.when(((0 <= df['CRSArrTime']) & (df['CRSArrTime'] <= 2400)),F.floor(df['CRSArrTime']/100) * 60 + df['CRSArrTime']%100).otherwise(-1))
 
 ############### END LOADING, FILTERING AND COMBINING ################
 
-# Trains a k-means model. ( this was for seeing we can make 2 clusters so we could binarize this 2 variables,
-# but correlation showed it was better to preserve them in its original codification, binarization code is in tools.py
-# inputCols = ['DepTime','ArrDelay']
-# file.write('Clustering'+str(inputCols)+'\n')
-# for i in range(2,6):
-#     info = kmeans_info(i,df,inputCols)
-#     file.write(info)
-#
-# inputCols=['CRSArrTime','ArrDelay']
-# file.write('Clustering'+str(inputCols)+'\n')
-# # Trains a k-means model.
-# for i in range(2,6):
-#     info = kmeans_info(i,df,inputCols)
-#     file.write(info)
 
-############### PREPARARE DF ################
+############### PREPARE DF ################
 
 # Prepare df to apply the algorithms
 inputcols = df.drop('ArrDelay').columns
 vectorAssembler = VectorAssembler(inputCols=inputcols, outputCol='features')
-df_ohe = vectorAssembler.transform(df)
-df_ohe = df_ohe.select(['features', 'ArrDelay'])
-df_ohe = df_ohe.withColumnRenamed('ArrDelay', 'label')
-
-df = df.drop(*cols_OHE)
-inputcols = df.drop('ArrDelay').columns
-vectorAssembler = VectorAssembler(inputCols=inputcols, outputCol='features')
 df = vectorAssembler.transform(df)
 df = df.select(['features', 'ArrDelay'])
-df = df.withColumnRenamed('ArrDelay', 'label')
+df = df.withColumnRenamed('ArrDelay','label')
 
 ################ ALGORITMOS ML ################
+# Linear Regression
 alg = LinearRegression()
-file.write('\n---> Linear Regression without OHE variables \n\n')
+file.write('\n---> Linear Regression \n\n')
 apply_LinearRegression(df, inputcols, file, alg)
-# file.write('\n---> Linear Regression with OHE variables \n\n')
-# apply_LinearRegression(df_ohe, inputcols, file, alg)
 
-# alg = GeneralizedLinearRegression()
-# file.write('\n---> Generalized Linear Regression without OHE variables \n\n')
-# apply_LinearRegression(df, inputcols, file, alg)
-# file.write('\n---> Generalized Linear Regression with OHE variables \n\n')
-# apply_LinearRegression(df_ohe, inputcols, file, alg)
-
+#Decision Tree Regressor
 alg = DecisionTreeRegressor()
-file.write('\n---> Decision Tree Regression without OHE variables \n\n')
+file.write('\n---> Decision Tree Regression \n\n')
 apply_TreeRegressor(df, file, alg)
-# file.write('\n---> Decision Tree Regression with OHE variables \n\n')
-# apply_TreeRegressor(df_ohe, file, alg)
 
+#Ranfom Forest Regressor
 alg = RandomForestRegressor()
-file.write('\n---> Random Forest Regression without OHE variables \n\n')
+file.write('\n---> Random Forest Regression \n\n')
 apply_TreeRegressor(df, file, alg)
-# file.write('\n---> Random Forest Regression with OHE variables \n\n')
-# apply_TreeRegressor(df_ohe, file, alg)
 
-alg = GBTRegressor()
-file.write('\n---> GBT Regression without OHE variables \n\n')
-apply_TreeRegressor(df, file, alg)
-# file.write('\n---> GBT Regression Regression with OHE variables \n\n')
-# apply_TreeRegressor(df_ohe, file, alg)
+file.close()
--- tools.py
+++ tools.py
@@ -1,44 +1,9 @@
 from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.stat import Correlation
-import pandas as pd
 from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.tuning import ParamGridBuilder,CrossValidator
-import numpy as np
-
-
-def kmeans_info(i, df, inputCols):
-    vecAssembler = VectorAssembler(inputCols=inputCols, outputCol='features')
-    df = vecAssembler.transform(df)
-    info = '---> With ' + str(i) + ' clusters \n'
-    kmeans = KMeans(k=i, seed=1)
-    model = kmeans.fit(df.select('features'))
-    # Make predictions
-    predictions = model.transform(df)
-    # Evaluate clustering by computing Silhouette score
-    evaluator = ClusteringEvaluator()
-    silhouette = evaluator.evaluate(predictions)
-    info += "Silhouette with squared euclidean distance = " + str(silhouette) + '\n'
-    info += "Cluster Centers: " + '\n'
-    ctr = []
-    centers = model.clusterCenters()
-    for center in centers:
-        ctr.append(center)
-        info += str(center) + '\n'
-    info += '--------------------\n'
-    return info
-
-
-def correlation_matrix(df, corr_columns, method='pearson'):
-    vector_col = "corr_features"
-    assembler = VectorAssembler(inputCols=corr_columns, outputCol=vector_col)
-    df_vector = assembler.transform(df).select(vector_col)
-    matrix = Correlation.corr(df_vector, vector_col, method)
-
-    result = matrix.collect()[0]["pearson({})".format(vector_col)].values
-    return pd.DataFrame(result.reshape(-1, len(corr_columns)), columns=corr_columns, index=corr_columns).to_string()
-
 
 def one_hot_encoder(df, inputCols, outputCols):
     tmpCols = [col + 'Indx' for col in inputCols]
@@ -67,7 +32,7 @@ def apply_LinearRegression(df, inputcols, file, alg):
     crossval = CrossValidator(estimator=alg,
                               estimatorParamMaps=paramGridlr,
                               evaluator=evaluator,
-                              numFolds=10)  # use 3+ folds in practice
+                              numFolds=10)
     # Run cross-validation, and choose the best set of parameters.
     cv_model = crossval.fit(df)
     best_model = cv_model.bestModel
@@ -85,34 +50,17 @@ def apply_TreeRegressor(df, file, alg):
     ### linear Regression
     # Paramethers
     paramGridlr = ParamGridBuilder() \
-        .addGrid(alg.maxDepth, [3, 5, 7, 10]) \
+        .addGrid(alg.maxBins, [20, 25, 30]) \
         .build()
     # Cross-validation (RMSE)
     evaluator = RegressionEvaluator(metricName='rmse')
     crossval = CrossValidator(estimator=alg,
                               estimatorParamMaps=paramGridlr,
                               evaluator=evaluator,
-                              numFolds=10)  # use 3+ folds in practice
+                              numFolds=10)
     # Run cross-validation, and choose the best set of parameters.
     cv_model = crossval.fit(df)
     best_model = cv_model.bestModel
-    best_maxDepth_param = best_model._java_obj.getMaxDepth()
+    best_maxBins_param = best_model._java_obj.getMaxBins()
     file.write('Minimum RMSE : ' + str(np.min(cv_model.avgMetrics)) + '\n')
-    file.write('MaxDepth value : ' + str(best_maxDepth_param) + '\n')
-
-# df = df.withColumn('DepTimeBinarized', F.when( ((700 <= df['DepTime']) & (df['DepTime'] <= 1400)) , 0).otherwise(1))
-# df = df.withColumn('CRSArrTimeBinarized', F.when( ((700 <= df['CRSArrTime']) & (df['CRSArrTime'] <= 1400)) , 0).otherwise(1))
-# # map strings to numbers with string indexer
-# stringIndexer = StringIndexer(inputCols=['DepTimeBinarized','CRSArrTimeBinarized'], outputCols=['DepTimeIndx','CRSArrTimeIndx'])
-# model = stringIndexer.fit(df)
-# df = model.transform(df)
-# # perform onehotencoding
-# encoder = OneHotEncoder(dropLast=False, inputCols=['DepTimeIndx','CRSArrTimeIndx'], outputCols=['DepTimeOHE','CRSArrTimeOHE'])
-# model= encoder.fit(df)
-# df = model.transform(df)
-# # dropping encoded columns
-# df = df.drop(*['DepTimeIndx','CRSArrTimeIndx'])
-#
-# df = df.withColumn('DepTime', F.when(((0 <= df['DepTime']) & (df['DepTime'] <= 2400)),F.floor(df['DepTime']/100) * 60 + df['DepTime']%100).otherwise(-1))
-# df = df.withColumn('CRSArrTime', F.when(((0 <= df['CRSArrTime']) & (df['CRSArrTime'] <= 2400)),F.floor(df['CRSArrTime']/100) * 60 + df['CRSArrTime']%100).otherwise(-1))
-# df = df.filter((df.DepTime != -1) & (df.CRSArrTime != -1) )
+    file.write('MaxBins value : ' + str(best_maxBins_param) + '\n')
